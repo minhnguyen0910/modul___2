0d7df8c1420bfc26028e32143eee5ed1f6ca8791
minhnguyen0910 <minhn091099@gmail.com>
2023-01-06 15:53:23 +0700

    first commit

---
+++ src/ss6_ke_thua/bai_tap/lop_point_va_moveable_point/MoveablePoint.java
@@ -0,0 +1,69 @@
+package ss6_ke_thua.bai_tap.lop_point_va_moveable_point;
+
+import ss3_mang.bai_tap.bai_1.Array;
+
+import java.util.Arrays;
+
+public class MoveablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint() {
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+
+    }
+
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
+    }
+
+    public float[] move() {
+        super.setX(super.getX() + xSpeed);
+        super.setY(super.getY() + ySpeed);
+        return new float[]{super.getX(), super.getY()};
+    }
+
+    public String toString() {
+        return Arrays.toString(move());
+    }
+
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint(5, 6, 8, 9);
+        moveablePoint.move();
+        System.out.println(moveablePoint);
+
+
+    }
+
+}
