af8c013ae787c2d1416e7a91e361290b30808051
minhnguyen0910 <minhn091099@gmail.com>
2023-01-31 21:53:46 +0700

    first commit

---
+++ src/ss11_generic_stack_queue/abc/Count.java
@@ -0,0 +1,23 @@
+package ss11_generic_stack_queue.abc;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Count {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter string :");
+        String string = scanner.nextLine();
+        String[] arr = string.toLowerCase().split("");
+        Map<String, Integer> map = new TreeMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!(map.containsKey(arr[i]))) {
+                map.put(arr[i], 1);
+            } else {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+        }
+        System.out.println(map);
+    }
+}
